c6757a362059d2fded18f7ac53b5f4bd49b5166d
Vinuth006 <vinuth1ga17cs173@gmail.com>
2022-01-25 15:51:29 +0530

    Adding Customer Details

---
+++ Vinuth B S/CustomersDetails/Models/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CustomersDetails.Models
+{
+    public class Customer
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string PanCardNo { get; set; }
+        public string AdharNumber { get; set; }
+        public string DOB { get; set; }
+    }
+}

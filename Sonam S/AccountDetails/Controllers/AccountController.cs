2618321fbc63fa0b7e9017d30e5cf0a46a814c5c
sonamsingh30 <sonam30singh1997@gmail.com>
2022-01-31 14:48:47 +0530

    Account details

---
+++ Sonam S/AccountDetails/Controllers/AccountController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AccountDetails.Controllers
+{
+    public class AccountController : Controller
+    {
+        AccountContext db = new AccountContext();
+
+
+        public ActionResult Index()
+
+        {
+            return View(db.Account.ToList());
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Account account)
+
+        {
+            if (ModelState.IsValid)
+            {
+                db.Account.Add(account);
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            return View(account);
+        }
+
+
+
+
+    }
+}
